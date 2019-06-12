327f45bfa59794e15d0f84d1a6bc495a83171998
Dario Jolo <dariojolo@gmail.com>
2019-06-11 22:17:49 -0300

    Se agrego el boton floating
    Se le dio funcionalidad funcionalidad al boton, se agrego la libreria Fluttertoast para poner toast

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,28 +1,38 @@
 import 'package:flutter/material.dart';
-
-class HomePage extends StatelessWidget{
-
-final estiloTexto = new TextStyle(fontSize: 25);
+import 'package:fluttertoast/fluttertoast.dart';
 
 
+class HomePage extends StatelessWidget {
+  final estiloTexto = new TextStyle(fontSize: 25);
+  
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       appBar: AppBar(
         title: Text('Titulo'),
         centerTitle: true,
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-          Text('Numero de taps', style:estiloTexto ),
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text('Numero de taps', style: estiloTexto),
           Text('0', style: estiloTexto),
-        ],)
-
-        ),
+        ],
+      )),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          Fluttertoast.showToast(
+              msg: "Se apreto el boton +",
+              toastLength: Toast.LENGTH_SHORT,
+              gravity: ToastGravity.BOTTOM,
+              backgroundColor: Colors.red,
+              textColor: Colors.white,
+              timeInSecForIos: 1);
+        },
+      ),
     );
   }
-
 }
